74bd1154e4f7763d9cfb14f693580db34e511002
Suresh Khatri <servlet2@msn.com>
2013-02-09 23:37:39 +1100

    added population of select

--- bs-js/src/main/javascript/module/app.js
+++ bs-js/src/main/javascript/module/app.js
@@ -1,35 +1,50 @@
-$(document).ready(function(){
-	
-	var adMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	var bsMonths = ["Baisakh", "Jestha", "Ashaad", "Shrawan", "Bhadra", "Ashwin", "Kartik", "Mangsir", "Poush", "Magh", "Falgun", "Chaitra"];
-	
-	for (var i=1970; i<=2100; i++) {
-		var adYear = i - 57;
-		$("#adYear").append(new Option(adYear, adYear));
-		$("#bsYear").append(new Option(i, i));
-	}
-	
-	var numberOfMonths = 12;
-	
-	for (var i=0; i < numberOfMonths; i++) {
-		$("#adMonth").append(new Option(adMonths[i], i));
-		$("#bsMonth").append(new Option(bsMonths[i], i));
-	}
-	
-	var adDates = 31;
-	
-	for (var i=0; i<adDates; i++) {
-		$("#bsDate").append(new Option(i+1, i+1));
-		$("#adDate").append(new Option(i+1, i+1));
-	}
-	$("#bsDate").append(new Option(32, 32));
-	
-	var yearVal = new Date().getYear();
-	if (yearVal<1000) {
-		yearVal = 1900 + yearVal;
-	}
-	$("#adYear").val(yearVal);
-	$("#bsYear").val(yearVal+57);
-	
-	
-});
+/*global document*/
+/*global jQuery*/
+/*global Option*/
+var app = {
+    START_YEAR : 1970,
+    END_YEAR : 2100,
+    DIFF_YEAR : 57,
+    MONTHS_IN_YEAR : 12,
+    AD_MAX_DAY : 31,
+    DATE_FIX_VAL : 1900
+};
+
+(function ($) {
+    "use strict";
+    $(document).ready(
+        function () {
+            var adMonths = [ "January", "February", "March", "April", "May", "June", "July", "August", "September",
+                    "October", "November", "December" ],
+                bsMonths = [ "Baisakh", "Jestha", "Ashaad", "Shrawan", "Bhadra", "Ashwin", "Kartik", "Mangsir",
+                    "Poush", "Magh", "Falgun", "Chaitra" ],
+                numberOfMonths = app.MONTHS_IN_YEAR,
+                adDates = app.AD_MAX_DAY,
+                yearVal = new Date().getYear(),
+                i = 0;
+            if (yearVal < 1000) {
+                yearVal = app.DATE_FIX_VAL + yearVal;
+            }
+
+            for (i = app.START_YEAR; i <= app.END_YEAR; i += 1) {
+                $("#adYear").append(new Option(i - app.DIFF_YEAR, i - app.DIFF_YEAR));
+                $("#bsYear").append(new Option(i, i));
+            }
+
+            for (i = 0; i < numberOfMonths; i += 1) {
+                $("#adMonth").append(new Option(adMonths[i], i));
+                $("#bsMonth").append(new Option(bsMonths[i], i));
+            }
+
+            for (i = 0; i < adDates; i += 1) {
+                $("#bsDate").append(new Option(i + 1, i + 1));
+                $("#adDate").append(new Option(i + 1, i + 1));
+            }
+            $("#bsDate").append(new Option(app.AD_MAX_DAY + 1, app.AD_MAX_DAY + 1));
+
+            $("#adYear").val(yearVal);
+            $("#bsYear").val(yearVal + app.DIFF_YEAR);
+
+        }
+    );
+}(jQuery));

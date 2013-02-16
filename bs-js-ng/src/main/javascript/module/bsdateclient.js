c4e3a7eaaf58c51e00dc53e2cbf5cae08c67d17f
Suresh Khatri <servlet2@msn.com>
2013-02-17 00:38:34 +1100

    added angular bind for select

--- bs-js-ng/src/main/javascript/module/bsdateclient.js
+++ bs-js-ng/src/main/javascript/module/bsdateclient.js
@@ -1,7 +1,65 @@
-function DateClient($scope) {
-    var yearVal =  new Date().getYear();;
-    if (yearVal < 1900) {
-        yearVal += 1900;
+function DateClient ($scope) {
+    var now = new Date();
+
+    var numberOfMonths = 12;
+    var daysInAMonth = 31;
+
+    $scope.years = function () {
+        var i, years = [];
+        for (i = app.START_YEAR; i <= app.END_YEAR; i += 1) {
+            years.push(i - app.DIFF_YEAR);
+        }
+        return years;
     }
-    $scope.yearVal = yearVal;
+
+    $scope.bsYears = function () {
+        var i, years = [];
+        for (i = app.START_YEAR; i <= app.END_YEAR; i += 1) {
+            years.push(i);
+        }
+        return years;
+    }
+
+    $scope.months = function () {
+        var i, months = [];
+        for (i = 0; i < numberOfMonths; i += 1) {
+            months.push(i);
+        }
+        return months;
+    };
+
+    $scope.bsMonths = function () {
+        var i, months = [];
+        for (i = 0; i < numberOfMonths; i += 1) {
+            months.push(i);
+        }
+        return months;
+    };
+
+    $scope.dates = function () {
+        var dates = [];
+        for (i = 0; i < daysInAMonth; i += 1) {
+            dates.push(i + 1);
+        }
+        return dates;
+    }
+
+    $scope.bsDates = function () {
+        var dates = [];
+        for (i = 0; i < daysInAMonth; i += 1) {
+            dates.push(i + 1);
+        }
+        dates.push(32);
+        return dates;
+    }
+
+    $scope.adYear = now.getYear() + 1900;
+    $scope.adMonth = now.getMonth();
+    $scope.adDate = now.getDate();
+
+    var bikram = bsCalculator.convertToBikram($scope.adYear, $scope.adMonth + 1, $scope.adDate);
+    
+    $scope.bsMonth = bikram.month - 1;
+    $scope.bsDate = bikram.date;
+    $scope.bsYear = bikram.year;
 }
